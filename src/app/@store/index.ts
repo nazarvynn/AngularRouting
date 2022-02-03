60c110f5599f6ac0ce31a68b82eb2c61166b1a6f
Nazar Vynnytskyi <nvynnytskyi@jappware.com>
2022-02-04 00:36:58 +0200

    update index

--- src/app/@store/index.ts
+++ src/app/@store/index.ts
@@ -2,15 +2,4 @@ import * as AppStateActions from './app-state.actions';
 
 export { AppStateActions };
 export * from './app-state';
-export * from './common';
-export * from './user';
-export * from './manifest-node';
-export * from './navigation';
-export * from './panel';
-export * from './cart';
-export * from './notifications';
-export * from './recent';
 export * from './todo';
-export * from './collaboration';
-export * from './widget-state';
-export * from './grid';
